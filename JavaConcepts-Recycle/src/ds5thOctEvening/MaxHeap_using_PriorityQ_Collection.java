922b1e50fe4b83afc694ad6630b941152ad6cd9e
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-09 21:20:02 +0530

    priority queue - qu.isEmpty() and  qu.poll() instead of iterator - Omprakash

--- JavaConcepts-Recycle/src/ds5thOctEvening/MaxHeap_using_PriorityQ_Collection.java
+++ JavaConcepts-Recycle/src/ds5thOctEvening/MaxHeap_using_PriorityQ_Collection.java
@@ -6,37 +6,36 @@ import java.util.PriorityQueue;
 
 public class MaxHeap_using_PriorityQ_Collection {
 	public static void main(String[] args) {
-		PriorityQueue<Integer> pqObj = new PriorityQueue<>(Collections.reverseOrder());
+		PriorityQueue<Integer> pqObj = new PriorityQueue<>();
 		PriorityQueue<Integer> pqObj2 = new PriorityQueue<>(Collections.reverseOrder());
 		
 		pqObj.add(10); 
 		pqObj.add(30);
-		pqObj.add(20);
-		pqObj.add(400);
+		pqObj.add(2);
+		pqObj.add(10000);
+		
 		//pqObj.add(121212);
-		Iterator it = pqObj.iterator();
-		while(it.hasNext())
-			System.out.print(it.next()+" ");
-		System.out.println("");
-		pqObj.poll();
-		Iterator it2 = pqObj.iterator();
-		while(it2.hasNext())
-			System.out.print(it2.next()+" ");
-		pqObj.remove();
-		System.out.println("");
-		
-		Iterator it3 = pqObj.iterator();
-		while(it3.hasNext())
-			System.out.print(it3.next()+" ");
-		
-		System.out.println("");
-		//store in array
-		Object[] arr = pqObj.toArray();
-		for(int i=0; i<arr.length; i++) {
-			System.out.println(arr[i].toString());
+		/*
+		 * Iterator it = pqObj.iterator(); while(it.hasNext())
+		 * System.out.print(it.next()+" ");
+		 */
+		while(!pqObj.isEmpty()) {
+			System.out.print(pqObj.poll()+" ");
+			//pqObj.poll();
 		}
 		
-		
+		/*
+		 * System.out.println(""); pqObj.poll(); Iterator it2 = pqObj.iterator();
+		 * while(it2.hasNext()) System.out.print(it2.next()+" "); pqObj.remove();
+		 * System.out.println("");
+		 * 
+		 * Iterator it3 = pqObj.iterator(); while(it3.hasNext())
+		 * System.out.print(it3.next()+" ");
+		 * 
+		 * System.out.println(""); //store in array Object[] arr = pqObj.toArray();
+		 * for(int i=0; i<arr.length; i++) { System.out.println(arr[i].toString()); }
+		 * 
+		 */
 		
 		
 		
