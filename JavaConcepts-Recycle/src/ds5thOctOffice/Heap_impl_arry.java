6d5b35dcc2761425dd7e362f275c067d6362d8b5
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-09 20:11:01 +0530

    error when left side of && condition is written right side - fixed

--- JavaConcepts-Recycle/src/ds5thOctOffice/Heap_impl_arry.java
+++ JavaConcepts-Recycle/src/ds5thOctOffice/Heap_impl_arry.java
@@ -1,22 +1,22 @@
 package ds5thOctOffice;
 
-public class Heap_impl_arry.java
+public class Heap_impl_arry
 {
 	public static void main (String[] args)
 	{
-		int[] ary = {2,1,11,23,4,111,2323,99,8};
+		int[] ary = {20,1,2,5,4,30,100,8};
 		int len = ary.length;
 		for(int i=0;i<len; i++)
 			System.out.print(ary[i]+" ");
 		System.out.println(len);
-		heapFun(ary, len);
+		buildHeap(ary, len);
 		for(int i=0;i<len; i++)
 			System.out.print(ary[i]+" ");
 	
 	}
-	static void heapFun(int[] ary, int len){
+	static void buildHeap(int[] ary, int len){
 		//find non leaf node : half - 1
-		int nn_index = (len/2) - 1;
+		int nn_index = (len/2);
 		//traverse till 0th index
 		for(int i = nn_index; i>=0; i--){
 			heapify(ary, len, i);
@@ -24,27 +24,28 @@ public class Heap_impl_arry.java
 		
 	}
 	
-	static void heapify(int[] ary, int len, int root_index){
+	static void heapify(int[] ary, int len, int index){
 		//1.find all left and right index -- 2*i+1, 2*i+2
-		int max_index = root_index;
-		int left = 2*root_index + 1;
-		int right = 2*root_index + 2;
+		int largest = index;
+		int left = 2*index + 1;
+		int right = 2*index + 2;
 		
 		
 		//2.find max value index 
-		if(ary[left] > ary[max_index] && left < len){
-			max_index = left;
+		if(left < len && ary[left] > ary[largest]){//IMP Note: left side of && if we right side then we wll get errors
+			largest = left;
 		}
-		if(ary[right] > ary[max_index] && right < len){
-			max_index = right;
+		if(right < len && ary[right] > ary[largest]){//Note : left side condition come 1st else sometime we will get error index out of range 
+			largest = right;
 		}
 		
 		//3. if max index value is different from root_index then max value is children 
-		if(max_index != root_index){//swap b/w latest max index and root index
-			int temp = ary[root_index];
-			ary[root_index] = ary[max_index];
-			ary[max_index] = temp;
-			heapify(ary, len, max_index);
+		if(largest != index){//swap b/w latest max index and root index
+			int temp = ary[index];
+			ary[index] = ary[largest];
+			ary[largest] = temp;
+			
+			heapify(ary, len, largest);
 		}
 	}
 }
