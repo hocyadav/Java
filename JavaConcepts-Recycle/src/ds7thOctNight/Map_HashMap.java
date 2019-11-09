8313d631d71196c8922e9060fc137a450245a868
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-09 20:04:32 +0530

    Map  - hashmap

---
+++ JavaConcepts-Recycle/src/ds7thOctNight/Map_HashMap.java
@@ -0,0 +1,36 @@
+package ds7thOctNight;
+// put(obj key, obj value), putAll(Map map),
+//remove(Object key), get(Object key), containsKey(Object key)
+//keySet() keys, entrySet() --keys+values
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Map_HashMap {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put("a", 1);
+		map.put("b", 2);
+		
+		//putAll()
+		
+		System.out.println(map);
+		
+		map.remove("a");
+		System.out.println(map);
+		
+		Set<Map.Entry<String, Integer>>  st = map.entrySet();
+		map.put("v", 12);
+		System.out.println(st);
+		for(Map.Entry<String, Integer> m:st) {
+			System.out.print(m.getKey()+" ");
+			System.out.print(m.getValue()+" ");
+			
+		}
+		
+		
+		
+	}
+}
