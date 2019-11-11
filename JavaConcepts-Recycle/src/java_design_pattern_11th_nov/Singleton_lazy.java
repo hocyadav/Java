198754c8e8b8c58c9f7527f3b95cfab6a772b64b
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-11 23:15:26 +0530

    Singleton java design pattern - impl

---
+++ JavaConcepts-Recycle/src/java_design_pattern_11th_nov/Singleton_lazy.java
@@ -0,0 +1,15 @@
+package java_design_pattern_11th_nov;
+
+//Singleton lazy initialization
+public class Singleton_lazy {
+	//1. create static instance obj
+	private static Singleton_lazy obj;
+	//2 make constructor private
+	private Singleton_lazy() {}
+	//3. public method to send obj of this method
+	public static Singleton_lazy getObj() {
+		if(obj == null)
+			obj = new Singleton_lazy();
+		return obj;
+	}
+}
---
+++ JavaConcepts-Recycle/src/java_design_pattern_11th_nov/Singleton_thread_safe_body.java
@@ -0,0 +1,21 @@
+package java_design_pattern_11th_nov;
+
+public class Singleton_thread_safe_body {
+	// private static obj
+	private static Singleton_thread_safe_body obj;
+	//private constructor
+	private Singleton_thread_safe_body() {
+		// TODO Auto-generated constructor stub
+	}
+	//public method to get obj
+	public static Singleton_thread_safe_body getObj() {
+		if(obj == null) {
+			synchronized (Singleton_thread_safe_method.class) {
+				if(obj == null)
+					obj = new Singleton_thread_safe_body();
+			}			
+		}
+		return obj;
+	}
+	
+}
