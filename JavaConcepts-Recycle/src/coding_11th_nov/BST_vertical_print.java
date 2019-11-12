ccaf5571dec7e32be18184a4bf5dd59d2ab8d1f8
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-12 11:23:22 +0530

    BST vertical order traversal / print

---
+++ JavaConcepts-Recycle/src/coding_11th_nov/BST_vertical_print.java
@@ -0,0 +1,127 @@
+package coding_11th_nov;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.Vector;
+
+/**
+ * 
+ * @author Hariom Yadav - Nov 11, 2019
+ *
+ */
+
+//data structure 
+class Node{
+	int data;
+	Node left, right;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+}
+
+//use of DS
+class BST{
+	Node root;
+
+	public void insert(int i) {
+		// TODO Auto-generated method stub
+		root = ins_rec(root, i);
+		
+	}
+
+	private Node ins_rec(Node root, int key) {
+		if(root == null) {//in this case root value change that why return type is Node
+			root = new Node(key);
+			return root;
+		}else {
+			if(key < root.data)//update left and right
+				root.left = ins_rec(root.left, key);
+			else
+				root.right = ins_rec(root.right, key);
+		}
+		return root;//after update left and right return root
+	}
+
+	public void inorder() {
+		// TODO Auto-generated method stub
+		System.out.print("Inorder : ");
+		inor_rec(root);
+		System.out.println();
+		
+	}
+	private void inor_rec(Node root) {
+		if(root == null) return;
+		else {
+			inor_rec(root.left);
+			System.out.print(root.data+" ");
+			inor_rec(root.right);
+		}
+		
+	}
+
+	
+	public void printVerticalBST() {
+		//declare hash map - with key and value as linked list
+		//HashMap<Integer, Vector<Node>> hmap = new HashMap<Integer, Vector<Node>>();
+		//take treemap that will be sorted
+		TreeMap<Integer, Vector<Integer>> hmap = new TreeMap<>();
+		//call inorder and save in this hmap
+		int distance = 0;//root as 0 distance , move left and -1, riht +1
+		verticalUtility(root, hmap, hmap.size(), distance);
+		
+		System.out.println("BST Vertical Order : ");
+		//print the tree map - entry wise
+		for(Entry<Integer, Vector<Integer>> t : hmap.entrySet()) {
+			System.out.println(t.getValue());
+		}
+		
+	}
+	/**
+	 * update hmap instance
+	 * @param hmap
+	 * @param size
+	 * @param distance
+	 */
+	private void verticalUtility(Node root, TreeMap<Integer,Vector<Integer>> hmap, int distance, int size) {
+		//get distance value from hasmap and return vector(linked list), 
+		if(root == null)
+			return;
+		
+		//store current data in dynamic array(Vector / LL) 
+		Vector<Integer> arrLL = hmap.get(distance);
+		if(arrLL == null) {//if list not present then create add data
+			arrLL = new Vector<Integer>();
+			arrLL.add(root.data);
+		}else//simple append with old data
+			arrLL.add(root.data);
+		
+		//now put these in hmap
+		hmap.put(distance, arrLL);//distance = 0 : mapped with arraylist that contain all node that have distance 0
+
+		//now call left side
+		
+		verticalUtility(root.left, hmap, distance-1, size);
+		//now call right side
+		verticalUtility(root.right, hmap, distance+1, size);
+		
+	}
+	
+}
+
+
+public class BST_vertical_print {
+	public static void main(String[] args) {
+		BST obj = new BST();
+		obj.insert(10);obj.insert(7);obj.insert(8);
+		obj.insert(13);
+		obj.insert(11);obj.insert(15);
+		obj.insert(12);
+		obj.insert(14);
+		obj.insert(9);
+		obj.inorder();
+		
+		obj.printVerticalBST();
+	}
+}
