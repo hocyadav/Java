2e5da551b8238d86e60281056370d4153dbb6da2
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-16 00:06:58 +0530

    Comparator - compare(o1,o2) , sort Student object

--- JavaConcepts-Recycle/src/collection_PQ_15th_nov/PriorityQueue_SortedSet.java
+++ JavaConcepts-Recycle/src/collection_PQ_15th_nov/PriorityQueue_SortedSet.java
@@ -2,6 +2,7 @@ package collection_PQ_15th_nov;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -19,8 +20,35 @@ public class PriorityQueue_SortedSet {
 		Collections.sort(list);//this will sort only primitive data type - https://www.youtube.com/watch?v=oAp4GYprVHM
 		//sort will only take that obj of class that has implemented Comparable interface
 		//since Integer, String all these primitive wrapper class has implemented this interface and internally written unimplemented class
-		//SO to send our obj , our class 1st need to implement Comparable interface then write logic for sorting in unimplemented method compareTo()
+		//Solution : SO to send our obj , our class 1st need to implement Comparable interface then write logic for sorting in unimplemented method compareTo()
 		System.out.println(list);
+		//m2 : what if that class is from somewehere else - then we cant update class , 
+		//so for that sort() will take 2 input 1st one is same class obj, 2nd one is logic to sort
+		
+		
+		//sort object (1st store in list and then call Collections.sort)
+		List<Student> sList = new ArrayList<>();
+		Student o1 = new Student(1, "hari");
+		Student o2 = new Student(30, "om");
+		Student o3 = new Student(4, "yadav");
+		
+		sList.add(o1); sList.add(o2); sList.add(o3);
+		
+		Comparator comparatorObj = new Comparator<Student>() {
+
+			@Override
+			public int compare(Student o1, Student o2) {
+				return (o1.roll > o2.roll)? 1:-1;
+			}
+			
+		};
+		System.out.println("sList obj 		:"+sList);
+		Collections.sort(sList, comparatorObj);//sort student object based on roll no
+		System.out.println("sList obj sorted	:"+sList);
+		
+		
+		
+		
 		
 	}
 
---
+++ JavaConcepts-Recycle/src/collection_PQ_15th_nov/Student.java
@@ -0,0 +1,11 @@
+package collection_PQ_15th_nov;
+
+public class Student {//access anywhere (inside package(folder) or any other folder)
+	int roll;//default
+	String name;//default
+	
+	public Student(int i, String n) {
+	roll = i;
+	name = n;
+	}
+}
