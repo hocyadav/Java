436c7817a5e15f2c2e19cac6dcd289867dc1a6c0
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-16 14:04:13 +0530

    update changes

---
+++ JavaConcepts-Recycle/src/access_modifier_class_level_16th_nov_package1/test1/Hariom.java
@@ -0,0 +1,10 @@
+package access_modifier_class_level_16th_nov_package1.test1;
+
+import access_modifier_class_level_16th_nov_package1.Class_A;
+
+
+public class Hariom {
+public static void main(String[] args) {
+	Class_A obj = new Class_A();
+}
+}
