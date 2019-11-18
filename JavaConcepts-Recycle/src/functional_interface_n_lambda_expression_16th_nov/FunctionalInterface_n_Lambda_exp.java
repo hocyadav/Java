bdacd708d4a75073ae4ee281af1a952ad68bcd83
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-18 08:22:55 +0530

    functional interface and lambda exp

--- JavaConcepts-Recycle/src/functional_interface_n_lambda_expression_16th_nov/FunctionalInterface_n_Lambda_exp.java
+++ JavaConcepts-Recycle/src/functional_interface_n_lambda_expression_16th_nov/FunctionalInterface_n_Lambda_exp.java
@@ -5,10 +5,15 @@ package functional_interface_n_lambda_expression_16th_nov;
  * @author Hariom Yadav - Nov 16, 2019
  *
  */
-
+//requirement for FI is min 1 abstract method , and as many as default method
 @FunctionalInterface//1. adda annotation , 2. add one method
 interface FunInterface{
-	void method();
+	void method();//1 abstract method
+	
+	//optional default method - as many as we can add
+	default void mehod1() {
+		System.out.println("default");
+	}
 }
 
 @FunctionalInterface//1. adda annotation , 2. add one method
@@ -27,6 +32,7 @@ public class FunctionalInterface_n_Lambda_exp {
 	public static void main(String[] args) {
 		FunInterface obj = () -> System.out.println("hi");//3.
 		obj.method();
+		obj.mehod1();
 		
 		FunInterface2 obj2 = (x) -> System.out.println("hi..."+x);//3.
 		obj2.method(2);//0th step take value from argument -> send to (x) then (x) send to body present after "->" line
