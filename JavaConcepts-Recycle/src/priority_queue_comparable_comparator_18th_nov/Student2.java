27555df71f78c26e9e7927778a1057d2a87d3196
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-18 09:40:40 +0530

    Priority Queue all methods, comparator, comparable

---
+++ JavaConcepts-Recycle/src/priority_queue_comparable_comparator_18th_nov/Student2.java
@@ -0,0 +1,11 @@
+package priority_queue_comparable_comparator_18th_nov;
+
+public class Student2 {
+	int roll;
+	String name;
+	
+	public Student2(int i, String s) {
+		roll = i;
+		name = s;
+	}
+}
