4640bb4982eb29d377e560738e8fe10f0bdbf62d
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-21 20:47:17 +0530

    stack impl Arry, LL, reverse int and string

---
+++ JavaConcepts-Recycle/src/ds_21st_nov/Stack_reverse_number.java
@@ -0,0 +1,33 @@
+package ds_21st_nov;
+
+import java.util.Stack;
+
+public class Stack_reverse_number {
+
+	public static void main(String[] args) {
+		int i = 1234;
+		System.out.println("Input 	: "+i);
+		int t = reverseIntUsingStack(i);
+		System.out.println("Output 	: "+t);
+	}
+
+	private static int reverseIntUsingStack(int i) {
+		Stack<Integer> st = new Stack<>();
+		
+		while(i>0) {
+			st.push(i % 10);//mod : last digit
+			i = i/10; // divide : except last digit
+		}
+		
+		//System.out.println(st);
+		
+		int reverse = 0;
+		int x = 1;
+		
+		while(!st.isEmpty()) {//or st.empty()
+			reverse = reverse + st.pop()*x;//adding number at 1st place - like link list reverse concept
+			x = x * 10;
+		}
+		return reverse;
+	}
+}
---
+++ JavaConcepts-Recycle/src/ds_21st_nov/Stack_reverse_string.java
@@ -0,0 +1,32 @@
+package ds_21st_nov;
+
+import java.util.Stack;
+
+public class Stack_reverse_string {
+
+	public static void main(String[] args) {
+		String str = "abcde";
+		System.out.println("Input 	: "+str);
+		String t = reverseStrUsingStack(str);
+		System.out.println("Outpit 	: "+t);
+		
+	}
+	
+	private static String reverseStrUsingStack(String str) {
+		Stack<Character> st = new Stack<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			st.push(str.charAt(i));
+		}
+		
+		//System.out.println(st);
+		StringBuilder sb = new StringBuilder();
+		while(!st.isEmpty()){
+			sb.append(st.pop());
+		}
+		//System.out.println(sb);
+		
+		return sb.toString();
+	}
+
+}
