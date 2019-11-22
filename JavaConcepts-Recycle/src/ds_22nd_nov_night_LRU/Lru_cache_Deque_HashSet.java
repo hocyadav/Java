55596f5848bd3abc235b33e115b45f5bb8ff43bc
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-22 21:14:18 +0530

    Queue, Deque, LRU cache(deque+set)

---
+++ JavaConcepts-Recycle/src/ds_22nd_nov_night_LRU/Lru_cache_Deque_HashSet.java
@@ -0,0 +1,77 @@
+package ds_22nd_nov_night_LRU;
+
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+/**
+ * 
+ * @author Hariom Yadav - Nov 22, 2019
+ *
+ */
+class LruCache{
+	//two data structure --> deque + hash set
+	//deque --> for cache
+	//hash set --> check key present in deque or not (coz searching in dequeu is n time)
+	
+	//know something
+	Deque<Integer> dq = new LinkedList<>();
+	HashSet<Integer> set = new HashSet<>();
+	int cSize = 4;
+	
+	//does something
+	public void refer(int key) {
+		if(! set.contains(key)) {//not in set -> that means not in DQ cache --> that means add in both DQ + set
+			//1. full then make space by removing
+			if(dq.size() == cSize) {//if deque size is full --> remove last from DQ + remove from set
+				int last = dq.removeLast();//remove last ele from DQ
+				set.remove(last);//remove reference from set
+			}
+			//2. add
+			dq.addFirst(key);
+			set.add(key);
+			
+		}else {//present in set -> that means present in deque cache
+			//1. make space - search + remove
+			Iterator<Integer> it = dq.iterator();
+			int index = 0;
+			while(it.hasNext()) {
+				index++;
+				if(it.next() == key) {
+					break;
+				}
+			}
+			dq.remove(index);
+			
+			//2. add
+			dq.addFirst(key);
+			set.add(key);
+		}
+	}
+	/**
+	 * print content of LRU Cache
+	 */
+	public void print() {
+		Iterator<Integer> it = dq.iterator();
+		System.out.print("LRU Cache : ");
+		while(it.hasNext()) 
+			System.out.print(it.next()+" ");
+				
+		System.out.println();
+	}
+	
+	
+}
+
+
+public class Lru_cache_Deque_HashSet {
+	public static void main(String[] args) {
+		LruCache obj = new LruCache();
+		obj.refer(1); obj.print();
+		obj.refer(2); obj.print();
+		obj.refer(4); obj.print();
+		obj.refer(1); obj.print();
+		obj.refer(5); obj.print();
+		obj.refer(1); obj.print();
+	}
+}
