103e4a49f7db609352fd018507e6cd1d3f35e8f0
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-25 08:33:03 +0530

    update

--- JavaConcepts-Recycle/src/access_modifier_class_level_16th_nov_package1/test1/Hariom.java
+++ JavaConcepts-Recycle/src/access_modifier_class_level_16th_nov_package1/test1/Hariom.java
@@ -1,10 +1,10 @@
 package access_modifier_class_level_16th_nov_package1.test1;
 
-import access_modifier_class_level_16th_nov_package1.Class_A;
+import access_modifier_class_level_16th_nov_package1.Class_A;//coz class A is default and cant visible in another package
 
 
 public class Hariom {
 public static void main(String[] args) {
-	Class_A obj = new Class_A();
+	Class_A obj = new Class_A();//class A is default so not visible here in another package
 }
 }
