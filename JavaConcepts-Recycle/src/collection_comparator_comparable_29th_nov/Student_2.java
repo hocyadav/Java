90e407ffd42ce4bde4212e9b06c314fddf4e13fd
Hariom Yadav <hariom.yadav@broadcom.com>
2019-11-29 09:21:02 +0530

    comparator and comparable interface - functional interface

---
+++ JavaConcepts-Recycle/src/collection_comparator_comparable_29th_nov/Student_2.java
@@ -0,0 +1,19 @@
+package collection_comparator_comparable_29th_nov;
+/**
+ * 
+ * @author Hariom Yadav | 29-Nov-2019
+ *
+ */
+public class Student_2 {
+	String name;
+	int roll;
+	
+	
+	public Student_2(String name, int roll) {
+		super();
+		this.name = name;
+		this.roll = roll;
+	}
+	
+	
+}
