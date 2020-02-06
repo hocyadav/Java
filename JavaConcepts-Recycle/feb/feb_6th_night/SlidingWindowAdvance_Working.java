40180298f04229a755a2782dcf46cd16fe3c414e
Hariom Yadav <hariom.yadav@broadcom.com>
2020-02-06 22:24:25 +0530

    sliding window concept using hashmap

--- JavaConcepts-Recycle/feb/feb_6th_night/SlidingWindowAdvance_Working.java
+++ JavaConcepts-Recycle/feb/feb_6th_night/SlidingWindowAdvance_Working.java
@@ -1,13 +1,56 @@
 package feb_6th_night;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SlidingWindowAdvance_Working {
 	public static void main(String[] args) {
-		String str = "aacd";
-		int k = 2;
+		String str = "aaccd";
+		int k = 3;
 		int t = slidingWindow(str, k);
 		System.out.println(t);
+		
+		int t2 = slidingWindow2(str, k);
+		System.out.println(t2);
+		
+		
 	}
 
+	//using hashmap
+	private static int slidingWindow2(String str, int k) {
+		if(str == null || str.length() == 0) {
+			return 0;
+		}
+		
+		Map<Character, Integer> map = new HashMap<>();
+		int distinct = 0;
+		int l = 0;
+		int maxWindowSize = 0;
+		//2 pointer
+		for(int r = 0; r < str.length(); r++) {
+			if(map.containsKey(str.charAt(r)) == false) {
+				distinct++;
+			}
+			map.put(str.charAt(r), map.getOrDefault(str.charAt(r), 0) + 1);//get value and add +1 or get default value 0 and + 1
+			
+			if(distinct > k) {
+				//each time get one value and decrement by 1
+				int val = map.get(str.charAt(l));
+				if(val > 0) {
+					map.put(str.charAt(l), map.get(str.charAt(l)) - 1);//decrement by 1
+				}else if(val == 0) {
+					distinct--;
+				}
+				
+				l++;
+				
+			}
+			maxWindowSize = Math.max(maxWindowSize, r - l + 1);
+		}
+		return maxWindowSize;
+	}
+
+	//using array as hashmap
 	private static int slidingWindow(String str, int k) {
 		//null check
 		if(str == null || str.length() == 0) {
@@ -23,8 +66,10 @@ public class SlidingWindowAdvance_Working {
 			map[str.charAt(r)]++;
 			
 			while(distinct > k) {//always check if distict element is greater than k or not coz we want k distict length
+				//take one value and decrement 1 each time
 				map[str.charAt(l)]--;//subtrating count as 1 from map
 				if(map[str.charAt(l)] == 0) distinct--;//if count for a given char is 0 then distinct cont also reduce
+				
 				l++;
 			}
 			maxWindowSize = Math.max(maxWindowSize, r - l + 1);
@@ -33,5 +78,6 @@ public class SlidingWindowAdvance_Working {
 	}
 }
 /*
-3
+5
+5
  */
