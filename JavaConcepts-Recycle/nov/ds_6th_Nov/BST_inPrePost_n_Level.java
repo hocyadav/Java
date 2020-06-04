c0c2c34b09c1088698d129387ba5741524c0dd37
Hariom Yadav <56931032+hocyadav@users.noreply.github.com>
2020-06-04 09:53:47 +0530

    Update BST_inPrePost_n_Level.java

--- JavaConcepts-Recycle/nov/ds_6th_Nov/BST_inPrePost_n_Level.java
+++ JavaConcepts-Recycle/nov/ds_6th_Nov/BST_inPrePost_n_Level.java
@@ -8,22 +8,20 @@ import java.util.Queue;
  *
  */
 
-//ds
+//1. "data structure" use for Tree
 class Node{
 	int data;
 	Node left, right;
-	//does something
+	
 	Node(int v){
-		data = v;
+	 data = v;
 	}
 }
 
+//2. data structure implementation
 class BST{
-	//know some
 	Node root;
-	
-	//does some
-	
+	//search key inside tree
 	public Node search(Node root, int key) {
 		if(root == null || root.data == key) {
 			return root;
@@ -36,12 +34,12 @@ class BST{
 	}
 	
 
-	
+	//TREE BFS - inorder , preorder, postorder
 	void inorder() {
 		inrec(root);
 	}
 	private void inrec(Node rt) {
-		if(rt == null) return;
+		if(rt == null) return;//null check , recursion base case
 		else {
 			inrec(rt.left);
 			System.out.print(rt.data+" ");
@@ -53,7 +51,7 @@ class BST{
 		prec(root);
 	}
 	private void prec(Node ro2) {
-		if(ro2 == null) return;
+		if(ro2 == null) return;//recursion base case
 		else {
 			System.out.print(ro2.data+" ");
 			prec(ro2.left);
@@ -65,7 +63,7 @@ class BST{
 		postRec(root);
 	}
 	private void postRec(Node pr) {
-		if(pr == null) return;
+		if(pr == null) return;//recursion base case
 		else {
 			postRec(pr.left);
 			postRec(pr.right);
@@ -73,33 +71,35 @@ class BST{
 		}
 	}
 	
+	//TREE DFS - also know as Level Order
 	void LevelOrder() {
-		//queue
-		Queue<Node> qobj = new LinkedList<Node>();
+		Queue<Node> qobj = new LinkedList<Node>();//queue
 		qobj.add(root);
 		
-		while(!qobj.isEmpty()) {
+		while(!qobj.isEmpty()) {//check until queue has value
+			//Remove from queue
 			Node temp = qobj.poll();
 			System.out.print(temp.data+" ");
 			
+			//Insert into queue
 			if(temp.left != null)
 				qobj.add(temp.left);
 			if(temp.right != null)
 				qobj.add(temp.right);
 		}
-		
 	}
 	
-	void insert(int key) {//root = : since root will change 
+	//insert new data correct position in Binary search tree
+	void insert(int key) {//Note : update root, since root will change
 		root = inrec(root, key);
-		
 	}
-	private Node inrec(Node ri,int v) {//go left or right - base case create node and return
-		
+	private Node inrec(Node ri,int v) {//base case create node and return --> go left or right
+		//Tree is empty : create new node + set data and return
 		if(ri == null) {
 			ri = new Node(v);
 			return ri;
 		}
+		//Tree is not empty : recursion : go left or right
 		if(v < ri.data) {
 			ri.left = inrec(ri.left, v);
 		}else {
@@ -107,10 +107,9 @@ class BST{
 		}
 		return ri;
 	}
-	
-	
 }
 
+//3. Test data structure implementation
 public class BST_inPrePost_n_Level {
 	public static void main(String[] args) {
 		BST obj = new BST();
